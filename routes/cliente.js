0ad670837c99eb13f3f91dc9a92043a20e6be253
Kevin <kevinroa71@gmail.com>
2020-07-02 01:14:01 -0400

    endpoints para la consulta y recarga de saldo

--- routes/cliente.js
+++ routes/cliente.js
@@ -6,6 +6,10 @@ const soap = require('soap');
 const app = express();
 
 
+// ###############
+//    Funciones
+// ###############
+
 function parseError(code, errors) {
     return {
         ok: false,
@@ -14,46 +18,80 @@ function parseError(code, errors) {
     };
 }
 
-
-app.post('/registro', (req, res) => {
-    const cliente = req.body;
-    const validator = new Validator();
-
-    validator(cliente).required().isObject((obj) => {
-        obj('documento').required().isString();
-        obj('nombres').required().isString();
-        obj('email').required().isString();
-        obj('celular').required().isString( );
-    });
-
+function runValidator(validator) {
     const errors = validator.run();
+    let mensajes = [];
 
     if (errors.length > 0) {
-        let mensajes = [];
         errors.forEach(error => {
             error.path.forEach(path => {
                 mensajes.push(`${path} is not valid. Reason: ${error.failed}`);
             });
         });
+    }
 
-        return res.status(400).json(parseError(400, mensajes));
+    return mensajes;
+}
+
+function makeRequest(req, res, constraint, method) {
+    const data = req.body;
+    const validator = new Validator();
+
+    validator(data).required().isObject(constraint);
+
+    const errors = runValidator(validator);
+
+    if (errors.length > 0) {
+        return res.status(400).json(parseError(400, errors));
     }
 
     soap.createClientAsync(process.env.SOAP_WLS)
-        .then(soapClient => {
-            return soapClient.registroClienteAsync(cliente);
-        })
+        .then(soapClient => soapClient[`${method}Async`](data))
         .then(result => {
             $json = JSON.parse(result[0].return.$value);
             if ($json) {
-                res.status($json.code).json($json);
+                return res.status($json.code).json($json);
             }
-            res.status(500).json(parseError(500, ["bad json to parse"]));
+            return res.status(500).json(parseError(500, ["bad json to parse"]));
         })
         .catch(err => {
-            res.status(500).json(parseError(500, [err.toString()]));
+            return res.status(500).json(parseError(500, [err.toString()]));
         });
+}
+
+
+// ###############
+// Rutas de la api
+// ###############
+
+app.post('/registro', (req, res) => {
+    const constraint = (obj) => {
+        obj('documento').required().isString();
+        obj('nombres').required().isString();
+        obj('email').required().isString();
+        obj('celular').required().isString();
+    };
+
+    return makeRequest(req, res, constraint, 'registroCliente');
 });
 
+app.put('/recargar', (req, res) => {
+    const constraint = (obj) => {
+        obj('documento').required().isString();
+        obj('celular').required().isString();
+        obj('valor').required().isNumber();
+    };
+
+    return makeRequest(req, res, constraint, 'recargarBilletera');
+});
+
+app.get('/consultar', (req, res) => {
+    const constraint = (obj) => {
+        obj('documento').required().isString();
+        obj('celular').required().isString();
+    };
+
+    return makeRequest(req, res, constraint, 'consultarSaldo');
+});
 
 module.exports = app;
